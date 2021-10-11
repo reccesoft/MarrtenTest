c03fd951040a90a0ee077723163def4b7c3683ed
reccesoft <87607185+reccesoft@users.noreply.github.com>
2021-10-11 10:45:23 +0200

    Add project files.

---
+++ MarrtenTest/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarrtenTest
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+    }
+}
